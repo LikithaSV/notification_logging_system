daff4a862ecb2c8770c4e7cf0ee97d4f7f4a999f
likitha0304 <likitha.savithri@1mg.com>
2024-08-05 12:14:33 +0530

    sms_manager file

--- app/managers/notification_manager.py
+++
@@ -1,7 +0,0 @@
-from app.repositories.notificaton_repo import NotificationRepo
-
-class NotificationManager:
-    @classmethod
-    async def create_notification(cls,payload):
-        response = await NotificationRepo.create_notification(payload)
-        return response
--- app/models/__init__.py
+++
@@ -1,3 +0,0 @@
-__all__ = ["Notification"]
-
-from app.models.Notification import Notification
